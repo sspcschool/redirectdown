67af868238d873a57c543b793265fcc80c2caac3
GamesThings <74464331+gamesthings@users.noreply.github.com>
2021-05-11 15:52:35 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+   <body>
+      <script>
+         setTimeout(function(){
+            window.location.href = 'https://sspcschool.github.io/maintenance';
+         }, 5000);
+      </script>
+      <p>Please wait 5 Seconds until redirected...</p>
+   </body>
+</html>
